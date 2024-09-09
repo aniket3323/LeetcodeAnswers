581419e66ac7290107681c1179e11ad9b5e68d9d
aniket3323 <aniketghodke3323@gmail.com>
2024-09-09 17:43:03 +0530

    Updated Water Bottle Solution

--- src/com/leet/waterBottles1518/Solution.java
+++ src/com/leet/waterBottles1518/Solution.java
@@ -1,17 +1,24 @@
 package com.leet.waterBottles1518;
 
 public class Solution {
-	
-	    public int numWaterBottles(int numBottles, int numExchange) {
-	    	
-	    	int temp = (numBottles/numExchange);
-	    	if()
-	    	if((temp > 0)) {
-	    		int maxBottles = 0;
-	    		maxBottles = maxBottles+ temp;
-	    	} 	
-			return numWaterBottles(temp,numExchange);
-	}
+    public int numWaterBottles(int numBottles, int numExchange) {
+        int totalDrank = numBottles; // initially, drink all full bottles
+
+        while (numBottles >= numExchange) {
+            int newBottles = numBottles / numExchange; // full bottles you get by exchanging
+            totalDrank += newBottles; // add these new bottles to total drank
+            numBottles = newBottles + (numBottles % numExchange); // remaining bottles after exchange
+        }
+
+        return totalDrank;
+    }
+
+    public static void main(String[] args) {
+        Solution solution = new Solution();
+        System.out.println(solution.numWaterBottles(9, 3));  // Output: 13
+    }
+}
+
 
 	
-}
+
